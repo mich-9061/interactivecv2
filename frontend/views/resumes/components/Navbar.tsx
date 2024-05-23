d952495b04e92fa0b1249e9e5f61b732fdbf6009
mich-9061 <jin199061@gmail.com>
2024-05-24 00:33:40 +0200

    feat(design): navbar

--- frontend/views/resumes/components/Navbar.tsx
+++ frontend/views/resumes/components/Navbar.tsx
@@ -1,18 +1,28 @@
 import { useSlug } from "Frontend/SlugContext";
-import { NavLink, Outlet } from "react-router-dom"
+import { NavLink, Outlet, useLocation } from "react-router-dom"
+import { LeftArrow } from "../icons/LeftArrow";
 
 export const Navbar = () => {
     const { slug } = useSlug();
+    const location = useLocation();
     return(
         <div className="flex justify-center">
             <div className="flex flex-col w-full max-w-7xl">
-                <div className="w-full flex flex-row justify-between py-1 px-28 bg-gray-100">
-                    <NavLink className="lowercase font-title lg:text-xl font-thin hover:underline" to={`/resume/${slug}`}>
-                        Interactive CV
-                    </NavLink>
-                    <NavLink className="lowercase font-title lg:text-xl font-thin hover:underline" to='/about'>
-                        about
-                    </NavLink>
+                <div className="w-full py-1 px-3 bg-gray-100">
+                    {location.pathname.includes("/about") ? (
+                        <NavLink className="flex flex-row items-center group relative" to={`/resume/${slug}`}>
+                            <div className="absolute left-[14px] group-hover:animate-bounceLeft transition-all flex items-center">
+                                <LeftArrow/>
+                            </div>
+                            <div className="lowercase font-title lg:text-xl font-thin group-hover:underline ml-12 transition-all duration-300">
+                                back
+                            </div> 
+                        </NavLink>
+                    ) : (
+                        <NavLink className="ml-12 lowercase font-title lg:text-xl font-thin hover:underline" to='/about'>
+                            about
+                        </NavLink>
+                    )}
                 </div>
                 <Outlet />
                 <div className='w-full h-9 bg-orange-600 px-6 py-2' />
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,35 @@ export default {
         title: ['Comfortaa'],
         name: ['Roboto Slab'],
         paragraph: ['Oxygen']
-      }
+      },
+      keyframes: {
+        bounceLeft: {
+          '0%,100%': { transform: 'translateX(-5px)' },
+          '25%': { transform: 'translateX(0px)' },
+          '50%': { transform: 'translateX(-5px)' },
+          '75%': { transform: 'translateX(0px)' },
+        },
+      },
+      animation: {
+        bounceLeft: 'bounceLeft 1.5s infinite',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        'html': { overflowY: 'scroll' },
+      });
+    },
+],
 }
+// @keyframes bounce {
+//   0%, 100% {
+//     transform: translateY(-25%);
+//     animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+//   }
+//   50% {
+//     transform: translateY(0);
+//     animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+//   }
+// }
